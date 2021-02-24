c70b1551237edd9a908407eaebd5b067c2054991
kirchhoff <twd4248298>
2021-02-24 17:27:23 +0800

    121 买卖股票的最佳时机 solution2

--- 121.go
+++ 121.go
@@ -24,7 +24,26 @@ func maxProfit(prices []int) (ret int) {
 	return
 }
 
+func maxProfit2(prices []int) (ret int) {
+	ret = 0
+	mxp := 0
+	mi := prices[0]
+	for i := 1; i < len(prices); i++ {
+		if prices[i]-mi > mxp {
+			mxp = prices[i] - mi
+		}
+		if prices[i] < mi {
+			mi = prices[i]
+		}
+	}
+	ret = mxp
+	return
+}
+
 func main() {
 	fmt.Println(maxProfit([]int{7, 1, 5, 3, 6, 4}))
 	fmt.Println(maxProfit([]int{7, 6, 4, 2, 1}))
+
+	fmt.Println(maxProfit2([]int{7, 1, 5, 3, 6, 4}))
+	fmt.Println(maxProfit2([]int{7, 6, 4, 2, 1}))
 }
